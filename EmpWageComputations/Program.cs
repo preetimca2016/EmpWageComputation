b645d8c74526aa1bbf96dc55a9fcd49fbc016178
“preetimca2016” <preetimca2016@gmail.com>
2022-09-03 23:13:52 +0530

    UC11-interfaceApproach

---
+++ EmpWageComputations/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EmpWageComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			EmpWageBuilder Realiance = new EmpWageBuilder("Realiance", 200, 150, 50);
+			Realiance.ComputeWage();
+			Console.WriteLine(Realiance.ToString());
+			EmpWageBuilder TATA = new EmpWageBuilder("TATA", 22, 20, 990);
+			TATA.ComputeWage();
+			Console.WriteLine(TATA.ToString());
+			
+		}
+    }
+}
